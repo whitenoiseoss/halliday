beddbebc5b7640e3514a91baeafdf93055a19555
whitenoiseoss <whitenoise.ossdev@gmail.com>
2018-04-22 23:01:33 -0500

    slight changes to errors library; ending for the night

--- errors/errors.go
+++ errors/errors.go
@@ -1,12 +1,24 @@
 package errors
 
-type ErrorType int
+import (
+	"fmt"
+)
 
+// ErrorType is a uint8 that makes sure we stay clean when passing errors around
+// There are 255 possible ErrorTypes
+type ErrorType uint8
+
+const (
+	ExampleError ErrorType = iota
+)
+
+// HallidayError holds an ErrorType as well as details of the error
 type HallidayError struct {
-	Type   ErrorType
-	Detail string
+	Category string
+	Type     ErrorType
+	Detail   string
 }
 
 func (he *HallidayError) Error() string {
-	return he.Detail
+	return fmt.Sprintf("urn:halliday:%s:%d : %s", he.Category, he.Type, he.Detail)
 }
