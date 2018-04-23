1dfe4aaf8fff94e3c0f003dac4883bedb417df3f
whitenoiseoss <whitenoise.ossdev@gmail.com>
2018-04-22 19:18:35 -0500

    update gitignore for vscode work

--- main.go
+++ main.go
@@ -29,8 +29,6 @@ func main() {
 	eid := Halliday.ECS.EntityManager.CreateEntity()
 	fmt.Println(eid)
 
-	// Halliday.ECS.ComponentRegistry.RegisterComponent("Test", Test{})
-
 	vm := Halliday.VMManager.VMPool.Get()
 	js, _ := vm.RunString(`
 		2+2
@@ -42,6 +40,7 @@ func main() {
 	vm.Set("NewTest", NewTest)
 	vm.Set("ComponentRegistry", Halliday.ECS.ComponentRegistry)
 
+	// Halliday.ECS.ComponentRegistry.RegisterComponent("Test", Test{})
 	_, _ = vm.RunString(`
 		ComponentRegistry.RegisterComponent("Test", NewTest());
 	`)
@@ -56,27 +55,4 @@ func main() {
 	testObj.Bark()
 
 	fmt.Println(Halliday.ECS.EntityManager.Len())
-	// vm := goja.New()
-	// ECSEngine := ecs.NewECSEngine()
-	// eid := ECSEngine.EntityManager.CreateEntity()
-	// eid2 := ECSEngine.EntityManager.CreateEntity()
-	// fmt.Println(eid)
-	// fmt.Println(eid2)
-	// fmt.Println(ECSEngine.EntityManager.Len())
-	// ECSEngine.ComponentRegistry.RegisterComponent("Test", Test{})
-	// T := ECSEngine.ComponentRegistry.Type("Test")
-	// fmt.Println(T)
-	// testObj := reflect.New(T).Interface().(*Test)
-	// testObj.Bark()
-
-	// vm.Set("ECS", ECSEngine)
-	// vm.Set("EntityManager", ECSEngine.EntityManager)
-	// js, _ := vm.RunString(`
-	// var eid = EntityManager.CreateEntity();
-	// EntityManager.GetEntityByID(eid);
-	// `)
-
-	// eid3 := js.Export().(*ecs.Entity)
-	// fmt.Println(eid3)
-	// fmt.Println(ECSEngine.EntityManager.Len())
 }
